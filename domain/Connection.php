5e6238d69a7827519a223fb203107d136d499691
Claudia Gardner-Pickett <claudiagardner-pickett@Claudias-Mac.fritz.box>
2020-05-03 13:35:57 +0100

    5th commit. Connecting frontend to backend with SESSION

--- domain/Connection.php
+++ domain/Connection.php
@@ -13,10 +13,7 @@ try {
     //always handle errors 
 }
 
-$pdo->setAttribute(
-        PDO::ATTR_ERRMODE,
-        PDO::ERRMODE_EXCEPTION);
-
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 //----------------- QUERY: Select all members
 
@@ -24,7 +21,10 @@ $stmt = $pdo->query("SELECT * FROM member", //query
         PDO::FETCH_OBJ);     //0th field 
 if (is_array($stmt) || is_object($stmt))
     foreach ($stmt as $row) {
-        var_dump($row);
+        echo $row->firstname . PHP_EOL;
+        echo $row->lastname . PHP_EOL;
+        echo $row->DoB . PHP_EOL;
+        echo $row->email . PHP_EOL;
     } unset($stmt);
 
 //----------------- QUERY: Select books from a specific genre
@@ -40,13 +40,11 @@ if (is_array($stmt) || is_object($stmt))
 //----------------- PREPARED STATEMENT: Inserting member information 
 
 $stmt = $pdo->prepare("INSERT INTO member (userid, firstname, lastname, DoB, email) VALUES (:userid, :firstname, :lastname, :DoB, :email)");
-$stmt->bindParam(':userid', $userid);
 $stmt->bindParam(':firstname', $firstname);
 $stmt->bindParam(':lastname', $lastname);
 $stmt->bindParam(':DoB', $DoB);
 $stmt->bindParam(':email', $email);
 
-$userid = 20;
 $firstname = "Sarah";
 $lastname = "Tooley";
 $DoB = "1992-08-12";
@@ -55,35 +53,35 @@ $stmt->execute();
 unset($stmt);
 
 //----------------- PREPARED STATEMENT: Inserting a book 
-//
-//$stmt = $pdo->prepare("INSERT INTO book (title,authorid,genreid,under18,publishdate,loanlength,quantity) VALUES (:title, :authorid, :genreid,:under18, :publishdate, :loanlength, :quantity)");
-//
-//$stmt->bindParam(":title", $title);
-//$stmt->bindParam(":authorid", $authorid);
-//$stmt->bindParam(":genreid", $genreid);
-//$stmt->bindParam(":under18", $under18);
-//$stmt->bindParam(":publishdate", $publishdate);
-//$stmt->bindParam(":loanlength", $loanlength);
-//$stmt->bindParam(":quantity", $quantity);
-//
-//$title = "On the Road";
-//$authorid = 2;
-//$genreid = 2;
-//$under18 = "No";
-//$publishdate = "1992-12-28";
-//$loanlength = 14;
-//$quantity = 5;
-//$stmt->execute();
-//unset($stmt);
+
+$stmt = $pdo->prepare("INSERT INTO book (title,authorid,genreid,under18,publishdate,loanlength,quantity) VALUES (:title, :authorid, :genreid,:under18, :publishdate, :loanlength, :quantity)");
+
+$stmt->bindParam(":title", $title);
+$stmt->bindParam(":authorid", $authorid);
+$stmt->bindParam(":genreid", $genreid);
+$stmt->bindParam(":under18", $under18);
+$stmt->bindParam(":publishdate", $publishdate);
+$stmt->bindParam(":loanlength", $loanlength);
+$stmt->bindParam(":quantity", $quantity);
+
+$title = "On the Road";
+$authorid = 2;
+$genreid = 2;
+$under18 = "No";
+$publishdate = "1992-12-28";
+$loanlength = 14;
+$quantity = 5;
+$stmt->execute();
+unset($stmt);
 
 //----------------- PREPARED STATEMENT: Update member's email
 
-//$stmt = $pdo->prepare("UPDATE member SET email = :email WHERE userid = :userid");
-//$stmt->bindParam(':userid', $userid);
-//$stmt->bindParam(':email', $email);
-//
-//$userid = 1;
-//$email = "s_smith@hotmail.com";
-//$stmt->execute();
-//unset($stmt);
+$stmt = $pdo->prepare("UPDATE member SET email = :email WHERE userid = :userid");
+$stmt->bindParam(':userid', $userid);
+$stmt->bindParam(':email', $email);
+
+$userid = 1;
+$email = "s_smith@hotmail.com";
+$stmt->execute();
+unset($stmt);
 
